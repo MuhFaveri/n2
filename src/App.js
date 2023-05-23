e1b5fca90bfec41eeb28493ffa0f168a17b16496
lucascalve <lucasr.calve@hotmail.com>
2023-05-22 22:22:19 -0300

    oficinas e palestras

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './medias.css'
 import { BrowserRouter as Router , Routes, Route} from 'react-router-dom'
 import Home from './components/home'
--- src/evento/PaginaEscrita.js
+++ src/evento/PaginaEscrita.js
@@ -1,7 +1,7 @@
 import Nav from "../components/Nav+header"
 import bg from './assets/Rectangle 17.png'
 import fotofoot from './assets/Rectangle 21.png'
-import Informacoes from "./informacoes"
+import Oficinas from "./oficinas"
 export default function PagEvento(){
 
 return(
@@ -9,7 +9,9 @@ return(
     <img src={bg} className="image-background" alt="aaaa"/>
     <Nav></Nav>
     <h1 id="aaaa"><b><i>Evento</i></b></h1>
-    <Informacoes></Informacoes>
+    
+    <Oficinas></Oficinas>
+
     <img src={fotofoot} id='fotofoot' alt="quebrou"/>
     </>
 )
